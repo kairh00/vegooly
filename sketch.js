e8dce465d504aa83706de0cd2bc139dfbb6bea53
kairh00 <65600128+kairh00@users.noreply.github.com>
2024-01-07 20:25:52 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,7 +2,9 @@ let tX;
 let tY;
 let tiledark = [];
 let tilelight = [];
-let count=0;
+let count;
+let bg;
+
 function preload() {
   for (let i = 0; i < 12; i++) {
     tiledark[i] = loadImage(`dark/slice${i}.png`);
@@ -11,18 +13,21 @@ function preload() {
     tilelight[j] = loadImage(`light/slice${j}.png`);
   }
 }
-function setup() {
-  createCanvas(600, 900);
+function setup() { 
+  createCanvas(displayWidth,displayHeight);
   frameRate(1);
-  ro = 8;
-  col = 6;
+  bg=createGraphics(width,height);
+  count=0;
+  ro = displayHeight/120;
+  col = displayWidth/120;
   cellw = width / col;
-  cellh = cellw;
+  cellh = cellw; 
 }
 function draw() {
- background(255,20,random(255));
+background('white');
+  image(bg,0,0);
+ bg.background(random(40,100),20,random(150,255)); blendMode(HARD_LIGHT);
   if (frameCount%2==0) {count++;}
-
   // c is column number, r is row number, t is cell number, m is remainder (to check if even or odd)
   for (let c = 0; c < col; c++) {
     for (let r = 0; r < ro; r++) {
@@ -30,15 +35,15 @@ function draw() {
       tX = int(random(0,12));
       tY= int(random(12,24));
       if (t%2 == 0) {
-       
-       image(tiledark[tX], cellw * c, cellh * r, cellw, cellh);
-        
+      image(tiledark[tX], cellw * c, cellh * r, cellw, cellh);
+        bg.noStroke();
+        bg.rect(cellw*c,cellh*r,cellw,cellh);
+        bg.fill(random(100,200),random(20,80),random(50,100)); 
       } 
       else {
         image(tilelight[tY], cellw * c, cellh * r, cellw, cellh);
-       
-      }
-  }
+      }   
+    }
   }
 
 }
